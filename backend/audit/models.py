dbe42400acec89cbfd433cfee0b7e12871d77673
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-05-28 01:11:05 +0530

    added audit/model.py

--- backend/audit/models.py
+++ backend/audit/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class AuditLog(models.Model):
+
+    action = models.CharField(max_length=255)
+
+    model_name = models.CharField(max_length=255)
+
+    record_id = models.IntegerField()
+
+    changed_by = models.CharField(max_length=255)
+
+    previous_data = models.JSONField(
+        null=True,
+        blank=True
+    )
+
+    new_data = models.JSONField(
+        null=True,
+        blank=True
+    )
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.action
